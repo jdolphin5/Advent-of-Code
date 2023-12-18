9db37debb111eba6898aab94ead8baa4bea95f1d
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-12-18 16:02:22 +1100

    day 10 - start

---
+++ 2023/day-10/FileDemo10.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class FileDemo10 {
+    public static void main(String[] args) {
+        File file = new File("2023/day-10/input.txt");
+        List<String> stringPerLineList = new ArrayList<>();
+
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine())
+                stringPerLineList.add(sc.nextLine());
+            sc.close();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+        int ret = 0;
+
+        
+
+        System.out.println(ret);
+    }
+}
