63809e4c010e1f4ae91d6e82e682335f51f794ca
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-01-16 22:45:29 +1100

    day 14 - 1

--- 2023/day-14/FileDemo14.java
+++ 2023/day-14/FileDemo14.java
@@ -2,6 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class FileDemo14 {
+
+    private static void tiltNorth(char[][] mat) {
+        for (int j = 0; j < mat[0].length; j++) {
+            for (int i = 1; i < mat.length; i++) {
+                if (i == 0 || mat[i-1][j] == '#' || mat[i-1][j] == 'O') {
+                    //do nothing
+                }
+                else {
+                    if (mat[i][j] == 'O') {
+                        mat[i][j] = '.';
+                        mat[i-1][j] = 'O';
+                        i-=2;
+                    }
+                }
+            }
+        }
+    }
+
+    private static long countWeight(char[][] mat) {
+        long ret = 0;
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 'O') {
+                    ret += (mat.length - i);
+                }
+            }
+        }
+
+        return ret;
+    }
+
     public static void main(String[] args) {
         File file = new File("2023/day-14/input.txt");
         List<String> stringPerLineList = new ArrayList<>();
@@ -15,8 +47,23 @@ public class FileDemo14 {
             System.out.println(e.toString());
         }
 
-        
+        char[][] mat = new char[stringPerLineList.size()][stringPerLineList.get(0).length()];
 
+        for (int i = 0; i < stringPerLineList.size(); i++) {
+            for (int j = 0; j < stringPerLineList.get(i).length(); j++) {
+                mat[i][j] = stringPerLineList.get(i).charAt(j);
+            }
+        }
+
+        tiltNorth(mat);
+
+        //for (int i = 0; i < mat.length; i++) {
+        //    System.out.println(Arrays.toString(mat[i]));
+        //}
+
+        long ret = countWeight(mat);
+
+        System.out.println(ret);
     }
     
 }
