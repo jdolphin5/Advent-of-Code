f798e32b59d5c5bedbd3c1e585435e61898e63c1
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-01-17 12:33:15 +1100

    day 15 - 1

--- 2023/day-15/FileDemo15.java
+++ 2023/day-15/FileDemo15.java
@@ -15,6 +15,33 @@ public class FileDemo15 {
             System.out.println(e.toString());
         }
 
+        List<String> initialisationSequence = new ArrayList<>();
+
+        for (int i = 0; i < stringPerLineList.size(); i++) {
+            String[] lineStrings = stringPerLineList.get(i).split(",", 0);
+
+            for (String s : lineStrings) {
+                if (s.length() > 0) {
+                    initialisationSequence.add(s);
+                }
+            }
+        }
+
+        long ret = 0;
+
+        for (String s : initialisationSequence) {
+            long cur = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                cur += (int)c;
+                cur *= 17;
+                cur %= 256;
+            }
+
+            ret += cur;
+            System.out.println(ret);
+        }
 
     }
 }
