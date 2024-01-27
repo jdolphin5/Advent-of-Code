04f7ac502c7e421a11c230454fbd4b1339cb7336
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-01-27 14:48:31 +1100

    day 21 - 2

--- 2023/day-21/FileDemo21.java
+++ 2023/day-21/FileDemo21.java
@@ -67,15 +67,57 @@ public class FileDemo21 {
             }
         }
 
-        boolean[][] afterMat = new boolean[mat.length][mat[0].length];
-        boolean[][][] visited = new boolean[mat.length][mat[0].length][65];
+        char[][] mat2 = new char[mat.length*9][mat[0].length*9];
+        for (int i = 0; i < mat2.length; i++) {
+            for (int j = 0; j < mat2[0].length; j++) {
+                mat2[i][j] = mat[i % mat.length][j % mat[0].length];
+            }
+        }
 
-        recur(mat, afterMat, visited, startI, startJ, 64);
+        startI = startI + 4 * mat.length;
+        startJ = startJ + 4 * mat[0].length;
 
         long ret = 0;
+
+        boolean[][] afterMat = new boolean[mat2.length][mat2[0].length];
+        boolean[][][] visited = new boolean[mat2.length][mat2[0].length][400];
+
+        recur(mat2, afterMat, visited, startI, startJ, 65);
 
         ret = countPositions(afterMat);
 
         System.out.println(ret);
+
+        boolean[][] afterMat2 = new boolean[mat2.length][mat2[0].length];
+        boolean[][][] visited2 = new boolean[mat2.length][mat2[0].length][400];
+
+        recur(mat2, afterMat2, visited2, startI, startJ, 131+65);
+
+        //26501365 steps = 202300 + 65
+
+
+        ret = countPositions(afterMat2);
+
+        System.out.println(ret);
+
+
+        boolean[][] afterMat3 = new boolean[mat2.length][mat2[0].length];
+        boolean[][][] visited3 = new boolean[mat2.length][mat2[0].length][400];
+
+        recur(mat2, afterMat3, visited3, startI, startJ, 131+131+65);
+
+        ret = countPositions(afterMat3);
+
+        System.out.println(ret);
+
+        //26501365 steps = 202300 + 65
+        //               = 202301 iterations
+
+        //Quadratic formula modelling the pattern using the results from (x = 65, x = 65 + 131, x = 65 + 131*2)
+        //3853 - 15527 x + 15615 x^2
+        //x = 202301
+        //x^2 = 40925694601
+
+        System.out.println(3853L - (15527L * 202301L) + (15615L * 40925694601L));
     }
 }
